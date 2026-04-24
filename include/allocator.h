dd197c03c51a56cb5739e0e920baede2434593f4
jdluques <juan.luque@utec.edu.pe>
2026-04-24 16:59:44 -0500

    feat: add macros to override standard memory allocation functions

--- include/allocator.h
+++ include/allocator.h
@@ -8,4 +8,9 @@ void  my_free(void *ptr);
 void *my_calloc(size_t nmemb, size_t size);
 void *my_realloc(void *ptr, size_t size);
 
+#define malloc  my_malloc
+#define free    my_free
+#define calloc  my_calloc
+#define realloc my_realloc
+
 #endif
